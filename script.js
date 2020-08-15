1a90f181a64fad0109d8efeb484e5a219ccbf3a7
niharikhaleesi <niroulaniharika@gmail.com>
2020-08-15 10:36:06 -0700

    Updated styling

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
     $(".sidenav").sidenav();
+    $(".modal").modal();
+});
+
+function callAjax()
+{
+    var queryURL = "https://www.themealdb.com/api/json/v1/1/random.php"
+    $.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function(response) {
+    console.log(response);
+$("#cocktail").text(response.meals[0].strMeal);
 });
 
 "mark added something here"
+
