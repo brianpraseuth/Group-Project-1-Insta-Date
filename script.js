5d9d28e2cedddfecdd5c616112959721b33324cc
niharikhaleesi <niroulaniharika@gmail.com>
2020-08-15 11:23:52 -0700

    Styled card and borders

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 $(document).ready(function(){
-    $(".sidenav").sidenav();
-    $(".modal").modal();
+    $(“.sidenav”).sidenav();
+    $(“.modal”).modal();
 });
-
-function callAjax()
-{
-    var queryURL = "https://www.themealdb.com/api/json/v1/1/random.php"
-    $.ajax({
-    url: queryURL,
-    method: "GET"
+var entreeQueryURL = “https://www.themealdb.com/api/json/v1/1/random.php”
+$.ajax({
+    url: entreeQueryURL,
+    method: “GET”
 }).then(function(response) {
     console.log(response);
-$("#cocktail").text(response.meals[0].strMeal);
+    $(“#entreeName”).text(response.meals[0].strMeal);
+    $(“#entreeImage”).attr(“src”, response.meals[0].strMealThumb);
+    $(“#entreeRecipe”).text(response.meals[0].strInstructions);
+    $(“#entreeLink”).attr(“href”, response.meals[0].strYoutube);
 });
-
-"mark added something here"
-
+var cocktailQueryURL = “https://www.thecocktaildb.com/api/json/v1/1/random.php”
+$.ajax({
+    url: cocktailQueryURL,
+    method: “GET”
+}).then(function(response) {
+    console.log(response);
+    $(“#cocktailName”).text(response.drinks[0].strDrink);
+    $(“#cocktailImage”).attr(“src”, response.drinks[0].strDrinkThumb);
+    $(“#cocktailRecipe”).text(response.drinks[0].strInstructions);
+    $(“#cocktailLink”).attr(“href”, response.drinks[0].strYoutube);
+});
