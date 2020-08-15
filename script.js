2d5fd2403a57f83a0ffae14fdfd73ded69846de7
Brian Praseuth <brianpraseuth@gmail.com>
2020-08-15 13:57:10 -0700

    Files with working application submitted. Although need to figure out how to align new image to the right

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
 $(document).ready(function(){
-    $(“.sidenav”).sidenav();
-    $(“.modal”).modal();
+    $(".sidenav").sidenav();
+    $(".modal").modal();
 });
-var entreeQueryURL = “https://www.themealdb.com/api/json/v1/1/random.php”
+
+var entreeQueryURL = "https://www.themealdb.com/api/json/v1/1/random.php"
+
 $.ajax({
     url: entreeQueryURL,
-    method: “GET”
+    method: "GET"
 }).then(function(response) {
     console.log(response);
-    $(“#entreeName”).text(response.meals[0].strMeal);
-    $(“#entreeImage”).attr(“src”, response.meals[0].strMealThumb);
-    $(“#entreeRecipe”).text(response.meals[0].strInstructions);
-    $(“#entreeLink”).attr(“href”, response.meals[0].strYoutube);
+    $("#entreeName").text(response.meals[0].strMeal);
+    $("#entreeImage").attr("src", response.meals[0].strMealThumb);
+    $("#entreeRecipe").text(response.meals[0].strInstructions);
+    $("#entreeLink").attr("href", response.meals[0].strYoutube);
+    
 });
-var cocktailQueryURL = “https://www.thecocktaildb.com/api/json/v1/1/random.php”
+
+var cocktailQueryURL = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+
 $.ajax({
     url: cocktailQueryURL,
-    method: “GET”
+    method: "GET"
 }).then(function(response) {
     console.log(response);
-    $(“#cocktailName”).text(response.drinks[0].strDrink);
-    $(“#cocktailImage”).attr(“src”, response.drinks[0].strDrinkThumb);
-    $(“#cocktailRecipe”).text(response.drinks[0].strInstructions);
-    $(“#cocktailLink”).attr(“href”, response.drinks[0].strYoutube);
+    $("#cocktailName").text(response.drinks[0].strDrink);
+    $("#cocktailImage").attr("src", response.drinks[0].strDrinkThumb);
+    $("#cocktailRecipe").text(response.drinks[0].strInstructions);
+    $("#cocktailLink").attr("href", response.drinks[0].strYoutube);
+    
 });
